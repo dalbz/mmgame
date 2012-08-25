9e21af0283f045985064b725d7c9f3440131ef02
Dave <david.albert09@gmail.com>
2012-08-25 12:01:58 -0400

    Added initial files

---
+++ server/server.js
@@ -0,0 +1,36 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
+
+client.on("error", function (err) {
+    console.log("error event - " + client.host + ":" + client.port + " - " + err);
+});
+
+client.select(10);
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
+
+
+var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");
+server.on("message", function (msg, rinfo) {
+  console.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port);
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
+
+server.bind(43278);
